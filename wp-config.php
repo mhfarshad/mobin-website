0b81bc26aab4e29a50b421874ce1ff4027c789a4
mhfarshad <43764892+mhfarshad@users.noreply.github.com>
2019-02-03 10:39:23 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -31,7 +31,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost:33066');
+define('DB_HOST', 'localhost:3066');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
